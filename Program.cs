96412f583bbf8111f7e7cca447ca54cecbf29133
Пользователь <@DESKTOP-E7N1RLV>
2023-12-26 19:31:40 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1. Определить, является ли число чётным или нечетным.
+            //Console.Write("Введите целое число: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //if (number%2 == 0)
+            //{
+            //    Console.WriteLine("Число четное.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Число нечетное.");
+            //}
+            //Задание 2. Определить, равны ли введенные два числа
+
+            //Console.Write("Введите первое число: ");
+            //int num1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Введите второе число: ");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //if (num1==num2)
+            //{
+            //    Console.WriteLine("Числа равны.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Числа не равны.");
+            //}
+            //Задание 3. Вводятся три числа. Нужно вывести «YES», если все числа равны, в противном случае - «NO».
+            //Console.Write("Введите первое число: ");
+            //int num1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Введите второе число: ");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Введите третье число: ");
+            //int num3 = int.Parse(Console.ReadLine());
+
+            //if (num1 == num2 && num3==num2)
+            //{
+            //    Console.WriteLine("YES");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("NO");
+            //}
+            //Задание 4: Дано целое число. Если оно является положительным, то прибавить к нему 1; в противном случае, вычесть из него 2. Вывести полученное число на экран.
+            //Console.Write("Введите целое число: ");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number > 0)
+            //{
+            //    number=number + 1;
+            //}
+            //else
+            //{
+            //    number=number - 2;
+            //}
+            //Console.WriteLine(number);
+            //Задание 5: Определить, является ли треугольник со сторонами a, b, c равносторонним.
+            //Console.Write("Введите длину стороны a: ");
+            //double a = double.Parse(Console.ReadLine());
+
+            //Console.Write("Введите длину стороны b: ");
+            //double b = double.Parse(Console.ReadLine());
+
+            //Console.Write("Введите длину стороны c: ");
+            //double c = double.Parse(Console.ReadLine());
+
+            //if (a==b && b==c)
+            //{
+            //    Console.WriteLine("Треугольник равносторонний.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Треугольник не является равносторонним.");
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
